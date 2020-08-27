e7812e0c190ce40a74fda5db6eb53df5186de864
Administrator <Administrator@EC2AMAZ-C4M7DF4>
2020-08-27 08:28:43 +0000

    sec commit

--- dotnetcoreapp/Startup.cs
+++ dotnetcoreapp/Startup.cs
@@ -32,7 +32,7 @@ namespace dotnetcoreapp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!!");
+                    await context.Response.WriteAsync("Hello welcome to demo!!");
                 });
             });
         }
